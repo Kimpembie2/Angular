ce21357a9b5a968c3a81d9230bcd585623a3d0f8
Kimpembie2 <biello.gabriele@iisgalvanimi.edu.it>
2024-03-21 13:55:00 +0000

    1

--- esercizio-6/src/app/classifica/classifica.component.ts
+++ esercizio-6/src/app/classifica/classifica.component.ts
@@ -13,6 +13,17 @@ import {MatDividerModule} from '@angular/material/divider';
 })
 export class ClassificaComponent {
 pro: any[] = [
-  {img: "https://d21is3bk1bus90.cloudfront.net/roster_avatars_v2/color/large/cyber.webp", nickname: "Cyber"}
+  {img: "https://prosettings.net/cdn-cgi/image/dpr=1%2Cf=auto%2Cfit=pad%2Cheight=675%2Cq=85%2Csharpen=2%2Cwidth=1200/wp-content/uploads/beaulo.png", nickname: "Beaulo", età: 22, nazione: 'Usa', operatore: "Valkyrie", news: "https://siege.gg/news/+Beaulo"  }
+  {img: "https://prosettings.net/wp-content/uploads/paluh.png", nickname: "Paluh", età: 24 , nazione: 'Brasil', operatore: "Sledge", news: "https://siege.gg/news/+Paluh?page"}
+  {img: "", nickname: "", età: , nazione: '', operatore: "", news: ""}
+  {img: "", nickname: "", età: , nazione: '', operatore: "", news: ""}
+  {img: "", nickname: "", età: , nazione: '', operatore: "", news: ""}
+  {img: "", nickname: "", età: , nazione: '', operatore: "", news: ""}
+  {img: "", nickname: "", età: , nazione: '', operatore: "", news: ""}
+  {img: "", nickname: "", età: , nazione: '', operatore: "", news: ""}
+  {img: "", nickname: "", età: , nazione: '', operatore: "", news: ""}
+  {img: "", nickname: "", età: , nazione: '', operatore: "", news: ""}
+
+
 ]
 }
